0b6d465453d9067c5b0981606d10799346189f14
Peter Ericson <pdericson@gmail.com>
2015-08-27 23:26:50 +1000

    entrypoint.sh: chmod

--- 0.22.1/ubuntu/14.04/entrypoint.sh
+++ 0.22.1/ubuntu/14.04/entrypoint.sh
@@ -3,6 +3,8 @@
 PRINCIPAL=${PRINCIPAL:-root}
 
 if [ -n "$SECRET" ]; then
+    touch /tmp/credential
+    chmod 600 /tmp/credential
     echo -n "$PRINCIPAL $SECRET" > /tmp/credential
     export MESOS_CREDENTIAL=/tmp/credential
 fi
