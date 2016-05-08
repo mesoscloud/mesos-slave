8e8747f55fffe174162f3d0551a66f525c6adcdb
Peter Ericson <pdericson@gmail.com>
2016-05-08 22:40:38 +1000

    lint

--- 0.24.1/centos/7/entrypoint.sh
+++ 0.24.1/centos/7/entrypoint.sh
@@ -5,7 +5,7 @@ PRINCIPAL=${PRINCIPAL:-root}
 if [ -n "$SECRET" ]; then
     touch /tmp/credential
     chmod 600 /tmp/credential
-    echo -n "$PRINCIPAL $SECRET" > /tmp/credential
+    printf '%s %s' "$PRINCIPAL" "$SECRET" > /tmp/credential
     export MESOS_CREDENTIAL=/tmp/credential
 fi
 
